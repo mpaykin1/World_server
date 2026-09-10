390a2f46a619d6dbdcb1aa20771403deaf71c936
mpaykin1 <120904647+mpaykin1@users.noreply.github.com>
2026-09-10 07:32:50 +0400

    feat: add IndieWorlds discovery foundation

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const mime = {
   '.mjs': 'text/javascript; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.xml': 'application/rss+xml; charset=utf-8',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
   '.jpeg': 'image/jpeg',
